91dd6ecace1b6c6d548f836f50360cfab14e529e
PolarBearITS <polar.bear.in.the.snow@gmail.com>
2017-03-11 20:41:42 -0500

    fixed broken commit

---
+++ Ichi-ni Fighting/Assets/init.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class init : MonoBehaviour {
+    GameObject f;
+    Camera cam;
+    float camWidth;
+    float xScale;
+    float yScale;
+    float zScale;
+
+	void Start () {
+        f = GameObject.Find("floor");
+        cam = Camera.main;
+        camWidth = cam.orthographicSize * cam.aspect / 2;
+        xScale = f.transform.localScale.x;
+        yScale = f.transform.localScale.y;
+        zScale = f.transform.localScale.z;
+	}
+    private void Update()
+    {
+        camWidth = cam.orthographicSize * cam.aspect / 2;
+        GameObject.Find("floor").transform.localScale = new Vector3(camWidth / xScale, yScale, zScale);
+        string w = getWinner();
+        if(w != "")
+        {
+            SceneManager.LoadScene("Start");
+        }
+    }
+
+    private string getWinner()
+    {
+        string w = "";
+        for(int i = 1; i < 3; i++)
+        {
+            if(w == "")
+            {
+                w = GameObject.Find("player" + i).GetComponent<master>().winner;
+            }
+        }
+        return w;
+    }
+}
---
+++ Ichi-ni Fighting/Assets/start.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject.Find("Loading").GetComponent<SpriteRenderer>().enabled = true;
+            SceneManager.LoadScene("Ichi-ni Fighting");
+        }
+	}
+}
